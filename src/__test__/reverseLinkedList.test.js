1448c7239a15ab621d4eff7ed2d17b68e352ef87
Kris3579 <kristianesvelt@hotmail.com>
2018-10-29 08:54:39 -0700

    false testing

--- src/__test__/reverseLinkedList.test.js
+++ src/__test__/reverseLinkedList.test.js
@@ -2,9 +2,46 @@
 
 const ReverseLinkedList = require('../reverseLinkedList');
 
-describe('countCharacters.js', () => {
-  test('Returned empty map because of empty string', () => {
-    const result = CountCharacters(testEmptyString);
-    expect(result.size).toEqual(0);
+const testLinkedList = new ReverseLinkedList(4);
+testLinkedList.insertAtHead(3);
+testLinkedList.insertAtHead(2);
+testLinkedList.insertAtHead(1);
+
+const testNotLL = [1, 2, 3, 4];
+
+describe('reverseLinkedList.js', () => {
+  test('Returned successfully reversed linked list using array method', () => {
+    const result = testLinkedList.reverseLinkedListWithArray(testLinkedList);
+    expect(result.head.value).toEqual(4);
+    expect(result.head.next.value).toEqual(3);
+    expect(result.head.next.next.value).toEqual(2);
+    expect(result.head.next.next.next.value).toEqual(1);
+  });
+  test('Returned undefined because of bad input array method', () => {
+    const result = testLinkedList.reverseLinkedListWithArray(testNotLL);
+    expect(result).toEqual(undefined);
+  });
+  test('Returned successfully reversed linked list using recursion method', () => {
+    const result = testLinkedList
+      .reverseLinkedListWithRecursion(testLinkedList, testLinkedList.head, null);
+    expect(result.head.value).toEqual(4);
+    expect(result.head.next.value).toEqual(3);
+    expect(result.head.next.next.value).toEqual(2);
+    expect(result.head.next.next.next.value).toEqual(1);
+  });
+  test('Returned undefined because of bad input recursion method', () => {
+    const result = testLinkedList.reverseLinkedListWithRecursion(testNotLL);
+    expect(result).toEqual(undefined);
+  });
+  test('Returned successfully reversed linked list by reordering the connections', () => {
+    const result = testLinkedList.reverseLinkedListByManualReorder(testLinkedList);
+    expect(result.head.value).toEqual(4);
+    expect(result.head.next.value).toEqual(3);
+    expect(result.head.next.next.value).toEqual(2);
+    expect(result.head.next.next.next.value).toEqual(1);
+  });
+  test('Returned undefined because of bad input reordering method', () => {
+    const result = testLinkedList.reverseLinkedListByManualReorder(testNotLL);
+    expect(result).toEqual(undefined);
   });
 });
--- src/reverseLinkedList.js
+++ src/reverseLinkedList.js
@@ -1,5 +1,81 @@
 'use strict';
 
-module.exports = function countCharacters(str) {
-    r
+const Node = require('./lib/node');
+
+module.exports = class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.currentNode = this.head;
+    while (this.currentNode.next) {
+      this.currentNode = this.currentNode.next;
+    }
+    this.tail = this.currentNode;
+  }
+
+  insertAtHead(value) {
+    const node = new Node(value); // O(1)
+
+    node.next = this.head; // O(1)
+    this.head = node; // O(1)
+    return this; // O(1)
+  }
+
+  reverseLinkedListWithArray(LL) {
+    if (!LL.head) {
+      return undefined;
+    }
+    const array = [];
+    let currentNode = LL.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    array.reverse();
+    const reversedLinkedList = new LinkedList(array[0]);
+    let reversedCurrentNode = reversedLinkedList.head;
+    for (let i = 1; i < array.length; i++) {
+      reversedCurrentNode.next = new Node(array[i]);
+      reversedCurrentNode = reversedCurrentNode.next;
+    }
+    return reversedLinkedList;
+  }
+
+  reverseLinkedListWithRecursion(LL, currentNode, prevNode) { // both O's(n)
+    if (!LL.head) {
+      return undefined;
+    }
+    LL.head = currentNode;
+    if (currentNode.next) {
+      this.reverseLinkedListWithRecursion(LL, currentNode.next, currentNode);
+    }
+    if (prevNode !== null) {
+      prevNode.next = null;
+    }
+    currentNode.next = prevNode;
+    return LL;
+  }
+
+  reverseLinkedListByManualReorder(LL) {
+    if (!LL.head) {
+      return undefined;
+    }
+    const saveTail = LL.tail;
+    while (LL.head !== LL.tail) {
+      let currentNode = LL.head;
+      while (currentNode.next) {
+        console.log(currentNode);
+        if (currentNode.next === LL.tail) {
+          currentNode.next = null;
+          LL.tail.next = currentNode;
+          LL.tail = currentNode;
+        }
+        if (currentNode.next === null) {
+          break;
+        }
+        currentNode = currentNode.next;
+      }
+    }
+    LL.head = saveTail;
+    return LL;
+  }
 };
